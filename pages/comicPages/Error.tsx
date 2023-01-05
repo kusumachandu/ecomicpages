800a3e65097464ff4fba05c8501b3ad11f9c4226
chandra shekar <kusumachandu43@gmail.com>
2023-01-05 12:12:42 +0530

    ✨Team page added, ⚡️ styling improvised

--- pages/comicPages/Error.tsx
+++ pages/comicPages/Error.tsx
@@ -6,8 +6,8 @@ export default function ErrorPage() {
         <div className="bg-black h-screen" style={{ backgroundImage: `url('https://www.dc.com/sites/default/files/Char_Gallery_Batman_DTC1018_6053f2162bdf03.97426416.jpg')` }}>
             <Navbar />
             <div className="flex justify-between w-[400px] m-auto mt-[650px]">
-                <button className="border-2 border-white bg-yellow-100 text-gray-700 font-bold w-44 rounded-lg transition-all active:scale-95">PRE-ORDER</button>
-                <button className="border-2 border-yellow-500 text-yellow-500 font-bold w-44 py-2 rounded-lg transition-all active:scale-95">GO BACK</button>
+                <button className="border-2 border-yellow-200 bg-yellow-200 text-black font-desc w-44 rounded-lg transition-all active:scale-95">PRE-ORDER</button>
+                <button className="border-2 border-yellow-200 text-yellow-200 font-desc  w-44 py-2 rounded-lg transition-all active:scale-95">GO BACK</button>
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Landing from './comicPages/Landing'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <>
       <div>
-        
+        <Landing />
       </div>
     </>
   )
