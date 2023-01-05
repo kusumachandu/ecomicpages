5f9029a679edb1b7c8d2e4d3c09b9031e0e1a12b
chandra shekar <kusumachandu43@gmail.com>
2023-01-05 16:20:24 +0530

    ..

--- pages/comicPages/Collection.tsx
+++ pages/comicPages/Collection.tsx
@@ -17,20 +17,20 @@ export default function CollectionPage(){
                 </div>
             </div>
             <div className="flex gap-10 pl-20 pt-10">
-                <div className="w-[200px] h-[250px] mt-12 active:scale-95 hover:scale-105 transition-all duration-200  rounded-lg">
-                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRbp5i8SlYG2HnW7WvLPJnYEU6wJ--Ryao5TBY8YhbbkKQ0kwQD0BodNj9F3xYX7U3oSU&usqp=CAU" />
+                <div className="w-[200px] h-[250px] mt-12 active:scale-95 hover:scale-105 transition-all duration-200  rounded-lg shadow-xl">
+                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRbp5i8SlYG2HnW7WvLPJnYEU6wJ--Ryao5TBY8YhbbkKQ0kwQD0BodNj9F3xYX7U3oSU&usqp=CAU" />
                 </div>
-                <div className="w-[225px] h-[275px] mt-[24px] active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg">
-                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-5gA3iCOVu32GnGf-QoBEGeM51-ymRzgy7Bi9AZL5znEtDVr5OHL1G-VnKN3sl5UO4hg&usqp=CAU" />
+                <div className="w-[225px] h-[275px] mt-[24px] active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg shadow-xl">
+                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-5gA3iCOVu32GnGf-QoBEGeM51-ymRzgy7Bi9AZL5znEtDVr5OHL1G-VnKN3sl5UO4hg&usqp=CAU" />
                 </div>
-                <div className="w-[250px] h-[300px] active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg">
-                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnxs66Wq9CjDxuj50PsKOZ3fIwOLLjyuTmTA&usqp=CAU" />
+                <div className="w-[250px] h-[300px] active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg shadow-xl">
+                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnxs66Wq9CjDxuj50PsKOZ3fIwOLLjyuTmTA&usqp=CAU" />
                 </div>
-                <div className="w-[225px] h-[275px] mt-[24px] active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg">
-                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMVUO8r3DoN7GGZ6O39cylzYMVqtaGObOpjA&usqp=CAU" />
+                <div className="w-[225px] h-[275px] mt-[24px] active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg shadow-xl">
+                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMVUO8r3DoN7GGZ6O39cylzYMVqtaGObOpjA&usqp=CAU" />
                 </div>
-                <div className="w-[200px] h-[250px] mt-12 active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg">
-                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMXZwaBfGvTCztdCE6mUCkoJdMiVfmKTh8DQ&usqp=CAU" />
+                <div className="w-[200px] h-[250px] mt-12 active:scale-95 hover:scale-105 transition-all duration-200 rounded-lg shadow-xl">
+                    <img className="w-[100%] h-[100%] cursor-pointer rounded-lg shadow-xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMXZwaBfGvTCztdCE6mUCkoJdMiVfmKTh8DQ&usqp=CAU" />
                 </div>
             </div>
             <div className="text-center font-desc text-yellow-200 pt-10">
