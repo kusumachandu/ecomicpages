221b200885c5cc8c05fe172eaed771c83a2903f6
chandra shekar <kusumachandu43@gmail.com>
2023-01-09 17:26:03 +0530

    ⚡️Improved responsiveness for landing page

--- pages/comicPages/Landing.tsx
+++ pages/comicPages/Landing.tsx
@@ -1,45 +1,43 @@
-import Link from "next/link"
+import Navbar from "../components/Navbar";
 
-
-export default function Landing() {
+export default function Landing(){
     return (
-        <div>
-            <div className="h-screen border pt-3 border-black lg:flex lg:gap-5 sm:relative lg:sticky bg-black rounded-md">
-                <div className="lg:w-[58%] sm:w-[100%] text-white lg:absolute">
-                    <a href="" className="absolute cursor-pointer text-4xl font-inter pt-8 pl-10 font-bold text-yellow-200">MOICI</a>
-                    <img className="w-[100%] pb-5 min-h-screen sm:opacity-[0.8] lg:opacity-[1]" src="https://cdn.dribbble.com/userupload/4083252/file/original-5a850202c0b00459951008a2190f2b70.jpg?compress=1&resize=1504x1128&vertical=center" />
+        <div className="bg-black rounded-lg">
+
+            <div>
+                <Navbar />
+            </div>
+            <div className="px-3 rounded-lg lg:flex lg:flex-grow lg:gap-x-5 lg:justify-between">
+
+                <div className=" w-full lg:w-[100%]  h-[325px] lg:h-[725px] md:h-[605px] rounded-lg">
+                    <img className="h-[100%] w-[100%] rounded-lg" src="https://cdn.dribbble.com/userupload/4083252/file/original-5a850202c0b00459951008a2190f2b70.jpg?compress=1&resize=1504x1128&vertical=center"/>
                 </div>
-                <div className="lg:w-[40%] sm:ml-[30%] lg:ml-[60%] sm:-mt-[111%] lg:mt-[0%] lg:relative sm:absolute  text-gray-300 font-desc">
-                    <div className="flex justify-between text-md font-semibold  lg:pl-44 lg:pr-10 lg:pt-10 sm:pl-28 sm:pr-2 sm:pt-5">
-                        <Link href='/comicPages/Collection' className="hovershake cursor-pointer">COLLECTION OPENING</Link>
-                        <Link href="/comicPages/About" className="hovershake cursor-pointer">ABOUT</Link>
-                        <Link href="/comicPages/Team" className="hovershake cursor-pointer">TEAM</Link>
-                        <button className="hovershake border border-yellow-100 text-yellow-100 w-24 rounded-md py-1 -mt-2 active:scale-95 transition-all ">APPLY</button>
+                <div className=" pt-10 lg:w-[65%]">
+                    <div className="text-white text-4xl lg:text-[54px] md:text-[60px]">
+                        <h1 className="font-inter text-center lg:text-start">HISTORY OF CREATION</h1>
                     </div>
-                    <div className="py-8 pl-2">
-                        <h1 className="font-bold lg:text-[64px] sm:text-[64px] font-inter lg:text-white sm:text-yellow-400">HISTORY OF CREATION</h1>
+                    <div className="text-yellow-400 font-desc pt-5 lg:px-2 md:w-[70%] md:m-auto lg:w-[100%]">
+                        <p className="text-center lg:text-start">WE WOULD LIKE TO TELL THE BLACKSTORY</p>
                     </div>
-                    <div className="pl-2 pb-8 text-yellow-100">
-                        <p className="font-desc text-[18px] font-light sm:text-white lg:text-yellow-100">WE WOULD LIKE TO TELL THE BLACKSTORY</p>
+                    <div className="text-yellow-400 font-desc py-1 px-[50px] lg:px-2 md:px-0 md:w-[60%] lg:w-[72%] lg:m-0 md:m-auto ">
+                        <p className="text-center lg:text-start">OUR TEAM IS MASTERING TECHNOLOGIES FOR NET UNIVERSES. AND WE DECIDED TO TRY TO OPEN NEW HORIZONS AND INTRODUCE NET INTO THE COMIC UNIVERSE! WE LOVE COMICS VERY MUCH AND WANTED TO PUT OUR SOUL INTO THIS PROJECT</p>
                     </div>
-                    <div className="pl-2 w-[430px] text-yellow-100">
-                        <p className="text-[18px] font-light font-desc sm:text-white lg:text-yellow-100">OUR TEAM IS MASTERING TECHNOLOGIES FOR NET UNIVERSES. AND WE DECIDED TO TRY TO OPEN NEW HORIZONS AND INTRODUCE NET INTO THE COMIC UNIVERSE! WE LOVE COMICS VERY MUCH AND WANTED TO PUT OUR SOUL INTO THIS PROJECT</p>
-                    </div>
-                    <div className="grid grid-cols-2 gap-x-4 gap-y-10  text-white pl-2 pt-8">
-                        <div className="hovershake h-[150px] rounded-lg shadow-lg">
-                            <img className="w-[100%] h-[100%] rounded-lg cursor-pointer hover:scale-105 transition-all duration-500" src="https://i.pinimg.com/564x/d6/68/83/d668839b939f18db97ebb350cba1be57.jpg" />
+                    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-10 lg:grid-cols-2 gap-y-10 lg:gap-y-2.5 lg:gap-x-5 w-[75%] lg:w-[100%] m-auto py-10">
+                        <div className="w-full h-52 rounded-lg cursor-pointer hovershake ">
+                            <img className="w-[100%] h-[100%] rounded-lg active:scale-95 hover:scale-105 transition-all duration-300" src="https://c4.wallpaperflare.com/wallpaper/541/426/525/iron-man-captain-america-fight-marvel-wallpaper-preview.jpg" />
                         </div>
-                        <div className="hovershake h-[150px] rounded-lg shadow-lg">
-                            <img className="w-[100%] h-[100%] rounded-lg cursor-pointer hover:scale-105 transition-all duration-500" src="https://i.pinimg.com/474x/16/02/b7/1602b76777919a1b684ad00674745399.jpg" />
+                        <div className="w-full h-52 rounded-lg cursor-pointer hovershake">
+                            <img className="w-[100%] h-[100%] rounded-lg active:scale-95 hover:scale-105 transition-all duration-300" src="https://c4.wallpaperflare.com/wallpaper/541/426/525/iron-man-captain-america-fight-marvel-wallpaper-preview.jpg" />
                         </div>
-                        <div className="hovershake rounded-lg h-[150px] shadow-lg">
-                            <img className="w-[100%] h-[100%] rounded-lg cursor-pointer hover:scale-105 transition-all duration-500" src="https://i.pinimg.com/474x/a4/fa/31/a4fa310a105f1f5a2c990e4412c56e9f.jpg" />
+                        <div className="w-full h-52 rounded-lg cursor-pointer hovershake lg:mt-4">
+                            <img className="w-[100%] h-[100%] rounded-lg active:scale-95 hover:scale-105 transition-all duration-300" src="https://c4.wallpaperflare.com/wallpaper/541/426/525/iron-man-captain-america-fight-marvel-wallpaper-preview.jpg" />
                         </div>
-                        <div className="hovershake rounded-lg h-[150px] shadow-lg">
-                            <img className="w-[100%] h-[100%] rounded-lg cursor-pointer hover:scale-105 transition-all duration-500" src="https://i.pinimg.com/474x/ae/6a/51/ae6a51155a593e664596fdd362d065e6.jpg"/>
+                        <div className="w-full h-52 rounded-lg cursor-pointer hovershake lg:mt-4">
+                            <img className="w-[100%] h-[100%] rounded-lg active:scale-95 hover:scale-105 transition-all duration-300" src="https://images.hdqwalls.com/wallpapers/avengers-endgame-final-ax.jpg" />
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -4,14 +4,16 @@ export default function Navbar() {
     return (
         <div className="flex justify-between">
             <div className="text-white pt-8 w-1/2">
-                <Link href='/comicPages/Landing' className="hovershake text-4xl cursor-pointer font-inter pl-10 font-bold text-yellow-100">MOICI</Link>   
+                <Link href='/comicPages/Landing' className="hovershake text-4xl cursor-pointer font-inter pl-5 lg:pl-20 md:pl-10 font-bold text-yellow-100">MOICI</Link>   
             </div>
             <div className="">
-                <div className="flex gap-5 text-md font-semibold pr-10 pt-10 text-gray-300 font-desc">
-                    <Link href='/comicPages/Collection' className="cursor-pointer hovershake">COLLECTION OPENING</Link>
+                <div className="flex gap-5 text-xs lg:text-[16px] font-semibold pr-2 lg:pr-20 md:pr-10 pt-10 lg:pt-12 text-gray-300 font-desc">
+                    <Link href='/comicPages/Collection' className="cursor-pointer hovershake w-[73px] lg:w-[110px]">COLLECTION OPENING</Link>
                     <Link href='/comicPages/About' className="cursor-pointer hovershake">ABOUT</Link>
                     <Link href="/comicPages/Team" className="cursor-pointer hovershake">TEAM</Link>
-                    <button className="hovershake border border-yellow-100 text-yellow-100 w-24 rounded-md py-1 -mt-2 active:scale-95 transition-all duration-200">APPLY</button>
+                    <div className="hovershake">
+                    <button className="border border-yellow-200 text-yellow-100 w-20 h-8 rounded-md py-1 -mt-2 active:scale-95 transition-all duration-200">APPLY</button>
+                    </div>
                 </div>
             </div>
         </div>
